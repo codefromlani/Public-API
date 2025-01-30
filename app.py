4c2f211a913a535edf0f11ff4da764fea6569fcb
Rodiat Hammed <rodiathammed48@gmail.com>
2025-01-30 11:08:53 +0100

    flask app

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, jsonify
+from flask import Flask, Response, jsonify
 from datetime import datetime
 import pytz
+import json
 from flask_cors import CORS
+from collections import OrderedDict
 
 app = Flask(__name__)
 CORS(app)
@@ -11,13 +13,14 @@ CORS(app)
 def get_info():
     current_time = datetime.now(pytz.UTC).strftime('%Y-%m-%dT%H:%M:%SZ')
 
-    info = {
-        "email": "rodiathammed48@gmail.com",
-        "current_datetime": current_time,
-        "github_url": "https://github.com/codefromlani/Public-API.git"
-    }
+    info = OrderedDict([
+        ("email", "rodiathammed48@gmail.com"),
+        ("current_datetime", current_time),
+        ("github_url", "https://github.com/codefromlani/Public-API")
+    ])
 
-    return jsonify(info)
+    json_response = json.dumps(info)
+    return Response(json_response, mimetype='application/json')
 
 @app.errorhandler(404)
 def not_found(error):
